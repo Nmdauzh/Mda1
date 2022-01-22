3e719e393c480e0343590d11f10645a86c2272cb
Nmdauzh <98192433+Nmdauzh@users.noreply.github.com>
2022-01-22 20:47:56 +0300

    Update Nastya.cs

--- Nastya.cs
+++ Nastya.cs
@@ -4,7 +4,7 @@ namespace Mda1
 {
     public static class Nastya      //Статическая Настя, которая проводит опрос животных
     {
-        public static void Show(Animal[] animals)
+        public static void Show(List<Animal> animals)
         {
             foreach (Animal animal in animals)
                 Console.WriteLine(animal.ToString() + animal.Сry() + '\n');
