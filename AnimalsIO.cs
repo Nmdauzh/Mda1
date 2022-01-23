eff85add29c71f40ea600c240074664d5e88c996
Nmdauzh <98192433+Nmdauzh@users.noreply.github.com>
2022-01-23 20:58:57 +0300

    Update AnimalsIO.cs

--- AnimalsIO.cs
+++ AnimalsIO.cs
@@ -7,7 +7,7 @@ namespace Mda1
     class AnimalsIO
     {
         public static void SaveToFile(List<Animal> animals, string path);
-        Console.Write($"Животное {Dog} Возраст {age} Вес {mass} Кости {bones}");
+        sw.Write($"Животное {Dog} Возраст {age} Вес {mass} Кости {bones}");
 
         public static List<Animal> GetFromFile(in string path);
         
